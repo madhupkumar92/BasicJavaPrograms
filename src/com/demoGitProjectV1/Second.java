e960e0dec963ce1c4ed2e76185402555c94176a8
91887 <91887@LAPTOP-DPJ4AEJC>
2021-04-19 01:20:12 +0530

    # WARNING: head commit changed in the meantime
    My 2nd commit

---
+++ src/com/demoGitProjectV1/Second.java
@@ -0,0 +1,12 @@
+package com.demoGitProjectV1;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is 2nd class");
+
+	}
+
+}
